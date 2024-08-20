c9216662e6244bf91bda680bad957e8930808bd9
sabarivelan2005 <sabarivelan.22ad@kct.ac.in>
2024-08-20 20:34:58 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,14 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('static'));
+
+app.get("/",(req,res)=>{res.render("forms.ejs")});
+app.listen(port,() => {
+console.log("server is started ...");
+});
